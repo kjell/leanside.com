2195fd286419c898874edf079497035cbeeefaa6
Kjell <kjell@leanside.com>
2014-01-10 23:28:04 -0600

    `jekyll build` service hook listener
    …in bash with netcat…

---
+++ _hook/hook.sh
@@ -0,0 +1,19 @@
+echo "starting hook: " $port " -- " $dest " -- " $branch
+
+[[ -e _pipe ]] && rm _pipe
+mkfifo _pipe
+while true; do
+  {
+    # block until netcat recieves a request and sends it to `_pipe`
+    read input < _pipe 
+    # only accept requests from github // "The Public IP addresses for these hooks are: 192.30.252.0/22"
+    if echo $input | egrep -q "192\.30\.252\.[0-255]"; then
+      echo $input >&2 # now do stuff
+      git checkout $branch
+      git fetch
+      git reset --hard origin/$branch >&2
+      ./_hook/bin/jekyll build -d $dest >&2
+      echo -e "HTTP/1.1 200 OK\r\n"
+    fi
+  } | nc -v -l $port &> _pipe
+done
