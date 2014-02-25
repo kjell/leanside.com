5da5cb9dc1d0800ecfae7a95e028cd86ed61c3c9
Kjell <kjell@leanside.com>
2014-02-24 23:40:52 -0600

    Fix hook: simpler grep, git to stderr

--- _hook/hook.sh
+++ _hook/hook.sh
@@ -7,10 +7,9 @@ while true; do
     # block until netcat recieves a request and sends it to `_pipe`
     read input < _pipe 
     # only accept requests from github // "The Public IP addresses for these hooks are: 192.30.252.0/22"
-    if echo $input | egrep -q "192\.30\.252\.[0-255]"; then
-      echo $input >&2 # now do stuff
-      git checkout $branch
-      git fetch
+    if echo $input | egrep -q "192\.30\.252" >&2; then
+      git checkout $branch >&2
+      git fetch >&2
       git reset --hard origin/$branch >&2
       ./_hook/bin/jekyll build -d $dest >&2
       echo -e "HTTP/1.1 200 OK\r\n"
